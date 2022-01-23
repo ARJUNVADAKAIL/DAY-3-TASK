f9f2795bd749a15758cf9bab56c5676754374b8a
ARJUNVADAKAIL <97578678+ARJUNVADAKAIL@users.noreply.github.com>
2022-01-23 16:32:47 +0530

    DAY 3 TASK
    For the given JSON iterate over all for loops (for, for in, for of, forEach)
    Create your own resume data in JSON format
    Read about the difference between window, screen and document in javascript

---
+++ resume.js
@@ -0,0 +1,21 @@
+var resume=[{
+     "Name":"ARJUN",
+     "DateOfBirth":`27.10.1996`,
+     "Email":`arjuns@gmail.com`,
+     "Education" : "Btech",
+     "Software Languages Known":["HTML","CSS" ,"JAVASCRIPT"],
+     "Experience":"2years",
+     "Languagesknown":["English", "Hindi"],
+     "Marital Status":"UnMarried",
+     "Nationality":"Indian",
+    }]
+    //for loop
+    for(var i=0;i<resume.length; i++){
+        console.log(resume[i])
+    }
+    //for in
+    for(var k in resume){
+        console.log(resume[k])
+    
+    }
+    
